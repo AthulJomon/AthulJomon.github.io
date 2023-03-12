0bf641bab4d03518437511550c79073ceee45b30
Athul Jomon <athuljomon10@gmail.com>
2023-03-12 21:03:44 +0530

    Script Edit

--- scripts.js
+++ scripts.js
@@ -2,27 +2,35 @@ function solve()
 {
     let num1=Number(document.getElementById("number1").value);
     let num2=Number(document.getElementById("number2").value);
-    let rem,ansH,ansL,temp1,temp2;
-    while(true){
-        if(num1>num2){
-            temp1=num1;
-            temp2=num2;
-        }
-        else{
-            temp1=num2;
-            temp2=num1;
-        }
-        rem=temp1%temp2;
-        if(rem==0){
-            ansH=temp2;
-            ansL=temp1*temp2/ansH;
-            break;
-        }
-        else{
-            temp1=temp2;
-            temp2=temp1%temp2;
-        }
+    let ansH,ansL,temp1,temp2;
+    
+    if(num1==0||num2==0){
+        ansH=num1!=0?num1:num2;
+        ansL=NaN;
+        print(num1,num2,ansH,ansL);
+        return;
     }
+
+    if(num1>num2){
+        temp1=num1;
+        temp2=num2;
+    }
+    else{
+        temp1=num2;
+        temp2=num1;
+    }
+
+    ansH=calculate(temp1,temp2);
+    ansL=num1*num2/ansH;
+    print(num1,num2,ansH,ansL);
+    return;
+}
+function calculate(a,b){
+    if(b==0)
+        return a;
+    calculate(b,a%b);
+}
+function print(num1,num2,ansH,ansL){
     document.getElementById("intro").innerHTML="The given inputs are "+num1+ " and "+num2;
     document.getElementById("answerH").innerHTML="HCF : "+ansH;
     document.getElementById("answerL").innerHTML="LCM : "+ansL;
