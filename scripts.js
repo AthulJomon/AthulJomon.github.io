d159255ce15a4ef1f3aa8b996bae9734df8f67f7
Athul Jomon <athuljomon10@gmail.com>
2023-03-12 19:51:01 +0530

    git commit -m "Script Edit Commit"

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
 function solve()
 {
-    let num1=document.getElementById("number1").value;
-    let num2=document.getElementById("number2").value;
-    let ansH,ansL;
-    let temp1=num1,temp2=num2;
+    let num1=Number(document.getElementById("number1").value);
+    let num2=Number(document.getElementById("number2").value);
+    let rem,ansH,ansL,temp1,temp2;
     while(true){
-        let q1=temp1%temp2;
-        if(q1==0){
+        if(num1>num2){
+            temp1=num1;
+            temp2=num2;
+        }
+        else{
+            temp1=num2;
+            temp2=num1;
+        }
+        rem=temp1%temp2;
+        if(rem==0){
             ansH=temp2;
             ansL=temp1*temp2/ansH;
             break;
         }
         else{
             temp1=temp2;
-            temp2=temp1/temp2;
+            temp2=temp1%temp2;
         }
     }
     document.getElementById("intro").innerHTML="The given inputs are "+num1+ " and "+num2;
